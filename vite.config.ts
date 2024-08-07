67c27f75bbe3f6c7df00df628129422d32ccb3f1
Ikhsan PUTRA <ikhsanputra@gmail.com>
2024-08-07 15:58:24 +0200

    add github page

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/landing-page-example",
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
