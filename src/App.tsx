06a7899cf250acfd3c703ce142c100bcba1e4952
Ikhsan PUTRA <ikhsanputra@gmail.com>
2024-08-07 12:51:47 +0200

    add Footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from "@/components/home";
 import Benefits from "@/components/benefits";
 import OurClasses from "@/components/ourClasses";
 import ContactUs from "@/components/contactUs";
+import Footer from "./components/footer";
 import { SelectedPage } from "@/shared/types";
 
 function App() {
@@ -36,6 +37,7 @@ function App() {
       <Benefits setSelectedPage={setSelectedPage} />
       <OurClasses setSelectedPage={setSelectedPage} />
       <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
     </div>
   );
 }
